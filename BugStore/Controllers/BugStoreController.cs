b92ebcace9d8503902a52240951685525cdb88d4
TRAN QUOC HUY <ltht1999@gmail.com>
2020-05-26 23:27:22 +0700

    ADD
    ssss

---
+++ BugStore/Controllers/BugStoreController.cs
@@ -0,0 +1,39 @@
+﻿using BugStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugStore.Controllers
+{
+    public class BugStoreController : Controller
+    {
+        DBBugstoreDataContext data = new DBBugstoreDataContext();
+        public ActionResult Index()
+        {
+            var product = Listfeatured();
+            return View(product);
+        }
+        private List<tbl_product> Listfeatured()
+        {
+            var ketqua = (from dp in data.tbl_products
+                         where dp.type_product == 1
+                         select dp).ToList();
+            return ketqua;
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BugStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BugStore.MvcApplication" Language="C#" %>
